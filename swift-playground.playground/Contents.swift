f6c924df4aee68eb54d64f43f4028b3516d8cb85
pwjablonski <pwjablonski@gmail.com>
2016-04-22 15:48:48 -0400

    Done.

--- swift-playground.playground/Contents.swift
+++ swift-playground.playground/Contents.swift
@@ -4,3 +4,26 @@ import UIKit
 
 var str = "Hello, playground"
 
+print(str)
+
+let flatiron = "the Flatiron School"
+
+var course = "Moblie Development with iOS"
+
+let leadInstructor = "Tim Clean"
+
+let students = 20
+
+var language = "Objective-C"
+
+var summary = "In the \(course) class at \(flatiron) there are \(students) learning \(language) from \(leadInstructor)"
+
+print(summary)
+
+language = "Swift"
+
+summary = "In the \(course) class at \(flatiron) there are \(students) learning \(language) from \(leadInstructor)"
+
+course = "Moblie Development Corps"
+
+summary = "In the \(course) class at \(flatiron) there are \(students) learning \(language) from \(leadInstructor)"
